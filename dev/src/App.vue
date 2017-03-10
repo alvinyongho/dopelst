a2e35930c6f328a1f829f206e9c99937bb11f4ac
David Luu <luu.david.c@gmail.com>
2017-03-10 10:26:48 -0800

    Navbar styling

--- dev/src/App.vue
+++ dev/src/App.vue
@@ -1,8 +1,10 @@
 <template>
   <div id="app">
-    <router-link to="/">/index</router-link>
-    <router-link to="/foo">/foo</router-link>
-    <router-link to="/bar">/bar</router-link>
+    <nav>
+      <router-link to="/">HOME</router-link><!--
+      --><router-link to="/foo" class="nav-link">FOO</router-link><!--
+      --><router-link to="/bar" class="nav-link">BAR</router-link>
+    </nav>
     <router-view></router-view>
   </div>
 </template>
@@ -14,12 +16,30 @@ export default {
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+body, html {
+  margin: 0;
+  padding: 0;
+
+  width: 100%;
+  height: 100%;
+}
+</style>
+
+<style scoped>
+nav {
+  background-color: #000;
+}
+
+nav a {
+  padding: 20px;
+
+  display: inline-block;
+
+  color: #fff;
+  text-decoration: none;
+}
+
+nav a.nav-link {
+  float: right;
 }
 </style>
