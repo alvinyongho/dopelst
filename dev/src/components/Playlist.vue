0f0c860b4d93e31d85a260942fcad7d8ddd242c2
David Luu <luu.david.c@gmail.com>
2017-03-11 10:46:06 -0800

    Reduced comment clutter in Playlist.vue

--- dev/src/components/Playlist.vue
+++ dev/src/components/Playlist.vue
@@ -229,15 +229,6 @@ section#playlist-view #header a {
   float: right;
 }
 
-/*
- * The following styles are auto-applied to elements with
- * transition="modal" when their visibility is toggled
- * by Vue.js.
- *
- * You can easily play with the modal transition by editing
- * these styles.
- */
-
 .modal-enter {
   opacity: 0;
 }
